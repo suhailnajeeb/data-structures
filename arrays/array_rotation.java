2150dd16d48b4eae70a4b1efed560c59f9bdae0c
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-02-21 08:54:10 +0600

    array rotation

---
+++ arrays/array_rotation.java
@@ -0,0 +1,41 @@
+
+// Write a function that takes an array A and an integer n. It rotates an array n times to the right direction and returns it. 
+
+public class array_rotation{
+
+    public static void main(String args[]){
+		int[] x = new int[10];
+		int i = 0;
+		while (i<x.length){
+			x[i] = i;
+			i++;
+		}
+		
+		rotate(x, 5);
+		
+		printArray(x);
+		       
+    }
+	
+	public static void printArray(int A[]){
+		for (int x : A)
+			System.out.println(x);
+	}
+		
+	public static void rotateRight(int array[]){
+		int lastElement = array[array.length - 1];
+		for (int i = array.length - 1; i > 0; i--)
+			array[i] = array[i-1];
+		array[0] = lastElement;
+	}
+	
+	public static int[] rotate(int A[], int n){
+		for (int i = 0; i < n; i++)
+			rotateRight(A)	;
+		return A;
+	}
+		
+}
+
+
+
