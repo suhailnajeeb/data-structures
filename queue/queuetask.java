29fc15fdc4b02e189855d5041f319d758d7a71e1
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-03-21 21:33:15 +0600

    create queue interface

---
+++ queue/queuetask.java
@@ -0,0 +1,11 @@
+public interface Queue{
+    int size();
+    boolean isEmpty();
+    void enqueue(Object o) throws QueueOverflowException;
+    Object pop() throws QueueUnderflowException;
+    Object peek() throws QueueUnderflowException;
+    String toString();
+    Object[] toArray();
+    int search(Object o);
+
+}
