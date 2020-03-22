c45d29fd32c077197f77b36fb06147a17fcbef73
Suhail Najeeb <suhailnajeeb19@gmail.com>
2020-03-23 00:13:36 +0600

    create patient

---
+++ queue/patient.java
@@ -0,0 +1,14 @@
+public class patient{
+    String name;
+    int age;
+    String bg;
+    int id;
+
+    patient(String name, int age, String bg){
+        this.name = name;
+        this.age = age;
+        this.bg = bg;
+        this.id = 0;
+    }
+
+}
